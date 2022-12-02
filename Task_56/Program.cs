0eb716f4a1415b26fb5020eff20b61b3fd889da6
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-12-02 23:47:19 +0200

    added task 56

---
+++ Task_56/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 56: Задайте двумерный массив. Напишите метод, 
+// который будет находить строку с наименьшей суммой элементов.
+
+bool IsParsedM = int.TryParse(Console.ReadLine(), out int m);
+bool IsParsedN = int.TryParse(Console.ReadLine(), out int n);
+if (!IsParsedM || !IsParsedN)
+{
+    Console.WriteLine("Error!");
+    return;
+}
+
+int[,] matrix = new int[m, n];
+FillMatrix(matrix);
+Print2DArray(matrix);
+Console.WriteLine();
+int[] arrayOfSum = GetSumOfColumn(matrix);
+PrintArray(arrayOfSum);
+Console.WriteLine();
+int valueOfMinColumn = FindMinColumnSum(arrayOfSum);
+Console.WriteLine(valueOfMinColumn);
+
+
+
+void FillMatrix(int[,] m)
+{
+    Random random = new Random();
+    for (int i = 0; i < m.GetLength(0); i++)
+    {
+        for (int j = 0; j < m.GetLength(1); j++)
+        {
+            m[i, j] = random.Next(1, 10);
+        }
+    }
+}
+
+void Print2DArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] GetSumOfColumn(int[,] array)
+{
+    int[] arrayOfSum = new int[m];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+            arrayOfSum[i] = sum;
+        }
+    }
+    return arrayOfSum;
+}
+
+int FindMinColumnSum(int[] a)
+{
+    int minValueOfColumn = a[0];
+    for (int i = 1; i < a.Length; i++)
+    {
+        if (a[i] < minValueOfColumn)
+        {
+            minValueOfColumn = a[i];
+        }
+    }
+    return minValueOfColumn;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
